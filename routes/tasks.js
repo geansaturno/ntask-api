2063bddc1c1b6fa4181ffdf028d384a79d19ce6b
Gean Saturno Gonçalves <cronosnaeg@gmail.com>
2016-07-17 23:58:28 -0300

    Work 17-07-01

--- routes/tasks.js
+++ routes/tasks.js
@@ -4,22 +4,18 @@ module.exports = app => {
     const Tasks = app.db.models.Tasks;
 
     app.route("/tasks")
-        .all((req, res) => {
-            delete req.body.id;
-            next();
-        })
         .get((req, res) => {
             Tasks.findAll({})
-                .then(tasks => {
-                    res.json({tasks: tasks})
-                .catch(error => {
-                    res.status(412).json({msg: error.message});
-                });
+            .then(tasks => {
+                res.json({tasks: tasks})
+            })
+            .catch(error => {
+                res.status(412).json({msg: error.message});
             });
         })
         .post((req, res) => {
             Tasks.create(req.body)
-                .then(result => {res.json(result))
+                .then(result => {res.json(result)})
                 .catch(error => {
                     res.status(412).json({msg: error.message});
                 })
@@ -27,10 +23,6 @@ module.exports = app => {
         })
     ;
     app.route("/tasks/:id")
-        .all((req, res) => {
-            delete req.body.id;
-            next();
-        })
         .get((req, res) => {
             Tasks.findOne({where: req.params})
                 .then(result => {
@@ -46,7 +38,7 @@ module.exports = app => {
             ;
         })
         .put((req, res) => {
-            Tasks.upate(req.body, {where: req.params})
+            Tasks.update(req.body, {where: req.params})
                 .then(result => res.sendStatus(204))
                 .catch(error => res.status(412).json({msg: error.message}))
         })
